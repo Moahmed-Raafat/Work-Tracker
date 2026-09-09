30c44b69a71c9e7ba310137672182b7657d351b6
https://github.com/Moahmed-Raafat/Note-Ultimate.git <moraafat28@gmail.com>
2026-09-09 23:30:42 +0300

    work items 17

--- app/src/main/java/com/example/worktracker/home/presentation/composables/WorkItemDetails.kt
+++ app/src/main/java/com/example/worktracker/home/presentation/composables/WorkItemDetails.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,8 +14,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -29,17 +35,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.worktracker.R
 import com.example.worktracker.common.Constants
+import com.example.worktracker.home.domain.model.WorkItem
 import com.example.worktracker.home.presentation.utils.ShareWorkItemIdViewModel
 import com.example.worktracker.home.presentation.viewmodel.get_work_item_by_id.GetWorkItemByIdViewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import java.nio.file.WatchEvent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -69,6 +81,9 @@ fun WorkItemDetails(navController: NavController,
         }
     }
 
+
+    var newTitle by remember { mutableStateOf<String>("") }
+    var newDescription by remember { mutableStateOf<String>("") }
 
     Scaffold(
         modifier = Modifier
@@ -128,7 +143,13 @@ fun WorkItemDetails(navController: NavController,
                         if (getWorkItemByIdResponse != null)
                         {
                             //todo Show the data
-                            Toast.makeText(context,getWorkItemByIdResponse.workItem.workItemNumber,Toast.LENGTH_SHORT).show()
+                            ShowInfo(workItem = getWorkItemByIdResponse.workItem,
+                                onChangingTitle = {
+                                    newTitle= it
+                                },
+                                onChangingDescription = {
+                                    newDescription= it
+                                })
                         }
 
                         // Show loading
@@ -166,5 +187,179 @@ fun WorkItemDetails(navController: NavController,
 
         }
     }
+}
+
+@Composable
+fun ShowInfo(workItem: WorkItem,
+             onChangingTitle: (String) -> Unit,
+             onChangingDescription: (String) -> Unit)
+{
+
+    var title by remember { mutableStateOf<String>(workItem.title) }
+    var description by remember { mutableStateOf<String>(workItem.description) }
+
+    Column(modifier = Modifier.fillMaxSize().padding(10.dp)) {
+
+        //number
+        Text(
+            text = workItem.workItemNumber,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.color_a)
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //title
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = colorResource(R.color.color_a))){
+                    append(Constants.TITLE) }
+                withStyle(style = SpanStyle(color = Color.Red)){
+                    append(" *") }
+            },
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        OutlinedTextField(
+            value = title,
+            onValueChange = {
+                title = it
+                onChangingTitle(it)
+                            },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text(text = Constants.ADD_TITLE) },
+            singleLine = false,
+            maxLines = 2,
+            shape = RoundedCornerShape(15.dp),
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //description
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = colorResource(R.color.color_a))){
+                    append(Constants.DESCRIPTION) }
+                withStyle(style = SpanStyle(color = Color.Red)){
+                    append(" *") }
+            },
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        OutlinedTextField(
+            value = description,
+            onValueChange = {
+                description = it
+                onChangingTitle(it)
+            },
+            modifier = Modifier.fillMaxWidth().height(100.dp),
+            label = { Text(text = Constants.ADD_DESCRIPTION) },
+            singleLine = false,
+            maxLines = 10,
+            shape = RoundedCornerShape(15.dp),
+            textStyle = LocalTextStyle.current.copy(
+                textAlign = TextAlign.Start
+            )
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //work type
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = Constants.WORK_TYPE + " :",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.color_a)
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Text(
+                text = workItem.workType?.name ?:"",
+                fontSize = 15.sp,
+                color = colorResource(R.color.color_b)
+            )
+
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //priority
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = Constants.PRIORITY + " :",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.color_a)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = workItem.priority?.name ?:"",
+                fontSize = 15.sp,
+                color = colorResource(R.color.color_b)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //status
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = Constants.STATUS + " :",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.color_a)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = workItem.status?.name ?:"",
+                fontSize = 15.sp,
+                color = colorResource(R.color.color_b)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //assignee
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = Constants.ASSIGNEE + " :",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.color_a)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = workItem.assignee?.name ?: "",
+                fontSize = 15.sp,
+                color = colorResource(R.color.color_b)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //assigner
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = Constants.ASSIGNER + " :",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.color_a)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = workItem.assigner?.name ?: "",
+                fontSize = 15.sp,
+                color = colorResource(R.color.color_b)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
+    }
+
 
 }
